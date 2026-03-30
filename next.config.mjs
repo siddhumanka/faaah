74dceaca430e9abd5e077b6946b491adb7ea964b
siddhumanka <siddhu.manka94@gmail.com>
2026-03-30 13:30:12 +0200

    Fix for cloudfare

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: async () => [
-    {
-      source: '/sounds/:path*',
-      headers: [
-        { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
-      ],
-    },
-  ],
+  output: 'export',
 };
 
 export default nextConfig;
